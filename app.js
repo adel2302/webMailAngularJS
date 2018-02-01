5010cc891fd2a54af87ac1d1d3faff4b1018566b
adel2302 <a.chouanine@hotmail.fr>
2018-02-01 22:32:23 +0100

    add custom filter

--- app.js
+++ app.js
@@ -195,4 +195,15 @@ angular
         }
       }
     );
+  })
+  .filter('surbrillanceRecherche', function() {
+    return function(input, recherche) {
+      if (recherche) {
+        return input.replace(
+          new RegExp('(' + recherche + ')', 'gi'),
+          "<span class='surbrillanceRecherche'>$1</span>"
+        );
+      }
+      return input;
+    };
   });
